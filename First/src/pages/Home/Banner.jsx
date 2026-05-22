9ac7e8753ee071e958f164aec9ef88203e67634b
Atiar-Rahman <atiar1709@gmail.com>
2026-05-22 21:22:56 +0600

    banner update

--- First/src/pages/Home/Banner.jsx
+++ First/src/pages/Home/Banner.jsx
@@ -1,12 +1,22 @@
 
 const Banner = () => {
     return (
-        <div>
-            <h1>Welcome to Our Website</h1>
-            <p>This is a simple banner component.</p>
-            <p>Feel free to customize it as needed.</p>
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Magni, suscipit?</p>
-        </div>
+        <div class="$$hero bg-base-200 min-h-screen">
+  <div class="$$hero-content flex-col lg:flex-row-reverse">
+    <img
+      src="https://img.daisyui.com/images/stock/photo-1635805737707-575885ab0820.webp"
+      class="max-w-sm rounded-lg shadow-2xl"
+    />
+    <div>
+      <h1 class="text-5xl font-bold">Box Office News!</h1>
+      <p class="py-6">
+        Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem
+        quasi. In deleniti eaque aut repudiandae et a id nisi.
+      </p>
+      <button class="$$btn $$btn-primary">Get Started</button>
+    </div>
+  </div>
+</div>
     );
 };
 
