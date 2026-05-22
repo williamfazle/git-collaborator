a44dbc143cf80d6c768e1987b811899443cfe609
fazle-rabby <fazlerabby272@gmail.com>
2026-05-22 21:15:01 +0600

    abc

---
+++ First/src/pages/Home/Banner.jsx
@@ -0,0 +1,13 @@
+
+const Banner = () => {
+    return (
+        <div>
+            <h1>Welcome to Our Website</h1>
+            <p>This is a simple banner component.</p>
+            <p>Feel free to customize it as needed.</p>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Magni, suscipit?</p>
+        </div>
+    );
+};
+
+export default Banner;
--- First/src/pages/Home/Home.jsx
+++ First/src/pages/Home/Home.jsx
@@ -1,8 +1,10 @@
+import Banner from "./Banner";
 
 
 const Home = () => {
     return (
         <div>
+            <Banner></Banner>
             <h1>Home Page</h1>
         </div>
     );
