6b2c900c27a6a38f79cf92c45f9336582709cc76
fazle-rabby <fazlerabby272@gmail.com>
2026-05-22 22:13:32 +0600

    Added Navbar with responsive

--- First/src/pages/Home/Home.jsx
+++ First/src/pages/Home/Home.jsx
@@ -1,10 +1,12 @@
 import Banner from "./Banner";
+import Nabvar from "../../layout/Nabvar";
 import Products from "./Products";
 
 
 const Home = () => {
     return (
         <div>
+            <Nabvar></Nabvar>
             <Banner></Banner>
             <Products></Products>
             <h1>Home Page</h1>
